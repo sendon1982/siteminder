eea4174209de57cf7341f059fae025fa490ca4c0
mjiang <mjiang@tripadvisor.com>
2019-07-23 17:14:19 +1000

    add new coin based controller

--- src/main/java/com/mjiang/email/MainAppRetry.java
+++ src/main/java/com/mjiang/email/MainAppRetry.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.Retryable;
 
-@SpringBootApplication
+//@SpringBootApplication
 @Retryable
 public class MainAppRetry implements CommandLineRunner {
 
